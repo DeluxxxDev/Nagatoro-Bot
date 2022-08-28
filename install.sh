ca8285d62555222b281db80145040de5a90ae3df
Azcurra <112267552+DeluxxxDev@users.noreply.github.com>
2022-08-28 04:43:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn
+pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn -y
 rm -rf node_modules
 yarn
-rm -rf sesi.json
+rm -rf qr.json
 npm start
 
 echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
